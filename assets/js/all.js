88aedc62f01c270dd6e56c4b5da2eb805e930898
lily <a0976198586@gmail.com>
2023-10-11 01:27:35 +0800

    Update 2023-10-10T17:27:33.738Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -91,5 +91,6 @@ var mapId = L.map('mapId', {
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(mapId);
+}).addTo(mapId); //-----------------------------------------------
+// 畫布按鈕開關
 //# sourceMappingURL=all.js.map
