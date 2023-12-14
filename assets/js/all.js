ce07217f0068b3ebc841e21b45ef2981df1ee8e3
lily <a0976198586@gmail.com>
2023-12-15 00:38:01 +0800

    Update 2023-12-14T16:37:59.113Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -76,7 +76,7 @@ var greenIcon = new L.Icon({
   shadowSize: [41, 41]
 });
 var redIcon = new L.Icon({
-  iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+  iconUrl: 'https://cdn.jsdelivr.net/gh/pointhi/leaflet-color-markers@master/img/marker-icon-2x-red.png',
   shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
   iconSize: [25, 41],
   iconAnchor: [12, 41],
